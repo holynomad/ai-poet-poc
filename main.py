020c376d4d55b529b2437d18878146d9091cd03b
Se Ha Lee <43846264+holynomad@users.noreply.github.com>
2024-09-24 22:04:58 +0900

    Update main.py fixed some comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-#2023-08-26 조코딩 온라인특강 : 랭체인 & streamlit 이용한 AI 웹서비스
+# 2023-08-26 조코딩 온라인 hands-on : 랭체인 & streamlit 이용한 AI 웹서비스
 
-#from dotenv import load_dotenv
-#load_dotenv() # api-key를 숨기는 도구 라이브러리
+# from dotenv import load_dotenv
+# load_dotenv() # api-key를 숨기는 도구 라이브러리
 
-#from langchain.chat_models import ChatOpenAI
-#chat_model = ChatOpenAI()
+# from langchain.chat_models import ChatOpenAI
+# chat_model = ChatOpenAI()
 
-#result = chat_model.predict("my name is macgyver !!")
-#print(result)
+# result = chat_model.predict("my name is macgyver !!")
+# print(result)
 
 
-#아래는 인공지능 시인 코드 using langchain + openAI api (유료)
+# 아래는 인공지능 시인 코드 using langchain + openAI api (유료)
 # from dotenv import load_dotenv
 # load_dotenv()
 # import streamlit as st
@@ -29,7 +29,7 @@
 
 
 
-#아래는 ggml + ctransformers 무료 인공지능 LLM 로컬환경 구축 (llama는 한글아직 약함..)
+# 아래는 ggml + ctransformers 무료 인공지능 LLM 로컬환경 구축 (llama는 한글아직 약함..)
 # import streamlit as st
 # from langchain.chat_models import ChatOpenAI
 # from langchain.llms import CTransformers
@@ -49,7 +49,7 @@
 #     st.write(result)
 
 
-#아래는 chatPDF using openAI API (유료) 모델
+# 아래는 chatPDF using openAI API (유료) 모델
 # from dotenv import load_dotenv
 # load_dotenv()
 # from langchain.document_loaders import PyPDFLoader
@@ -63,11 +63,11 @@
 # import tempfile
 # import os
 
-# #제목
+# # 제목
 # st.title("ChatPDF")
 # st.write("---")
 
-# #파일 업로드
+# # 파일 업로드
 # uploaded_file = st.file_uploader("Choose a file")
 # st.write("---")
 
@@ -80,7 +80,7 @@
 #     pages = loader.load_and_split()
 #     return pages
 
-# #업로드 되면 동작하는 코드
+# # 업로드 되면 동작하는 코드
 # if uploaded_file is not None:
 #     pages = pdf_to_document(uploaded_file)
 
@@ -112,7 +112,7 @@
 #             st.write(result["result"])
 
 
-#아래는 chatPDF 최종 수익화 모델(?).. 정확히 BM이 무엇인지는 모르겠음
+# 아래는 chatPDF 최종 수익화 모델(?).. 정확히 BM이 무엇인지는 모르겠음
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
@@ -130,14 +130,14 @@ from streamlit_extras.buy_me_a_coffee import button
 
 button(username="jocoding", floating=True, width=221)
 
-#제목
+# 제목
 st.title("ChatPDF")
 st.write("---")
 
-#OpenAI KEY 입력 받기
+# OpenAI KEY 입력 받기
 openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
-#파일 업로드
+# 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
 st.write("---")
 
@@ -150,7 +150,7 @@ def pdf_to_document(uploaded_file):
     pages = loader.load_and_split()
     return pages
 
-#업로드 되면 동작하는 코드
+# 업로드 되면 동작하는 코드
 if uploaded_file is not None:
     pages = pdf_to_document(uploaded_file)
 
